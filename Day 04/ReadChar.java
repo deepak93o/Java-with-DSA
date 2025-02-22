1996e38b6653e8cd77ca124e87121a97dad51ec6
Deepak <deepekbhatt455@gmail.com>
2025-02-22 21:45:53 +0530

    Read character

--- Day 04/ReadChar.java
+++ Day 04/ReadChar.java
@@ -4,7 +4,15 @@ public class ReadChar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        char ch = sc.next().charAt(0);
-        System.out.println("Character is: "+ ch);
+        // char ch = sc.next().charAt(0);
+        // System.out.println("Character is: "+ ch);
+
+        // String name = sc.next();
+        // System.out.println(name);
+
+        String p = sc.next();
+        int q = sc.nextInt();
+        System.out.print(p + " " + q);
+        // System.out.println(q);
     }
 }
