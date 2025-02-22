85ba5f5ec9aa9b994204dce9f8c834634bc073f0
Deepak <deepekbhatt455@gmail.com>
2025-02-22 22:02:57 +0530

    power

---
+++ Day 04/power.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class power {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = sc.nextInt();
+        System.out.print("Enter b: ");
+        int b = sc.nextInt();
+        int ans = 1;
+        for(int i = 1; i<= b; i++){
+            ans = ans * a;
+        }
+        System.out.println("a power b : " + ans);
+    }
+}
